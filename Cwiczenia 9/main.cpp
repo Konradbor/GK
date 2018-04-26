f6c88ab1886ac84bf5636865150e9a49baa9f04a
Your Name <you@example.com>
2018-04-26 09:02:33 +0200

    Ćw 9

--- Cwiczenia 9/main.cpp
+++ Cwiczenia 9/main.cpp
@@ -8,14 +8,15 @@
 * Email: marceniuk@yahoo.com
 */
 
-#include <windows.h> // plik nagłówkowy dla Windows
+//#include <windows.h> // plik nagłówkowy dla Windows
 #include <math.h> // plik nagłówkowy dla biblioteki matematycznej
-#include <stdio.h> // plik nagłówkowy dla we/wy standardowego
-#include <stdlib.h> // plik nagłówkowy dla bibioteki standardowej
-#include <gl\gl.h> // plik nagłówkowy dla biblioteki OpenGL32
-#include <gl\glu.h> // plik nagłówkowy dla biblioteki GLu32
-#include <GL\glut.h> // plik nagłówkowy dla biblioteki Glut
-#include <GL\glext.h>
+//#include <cmath>
+#include <stdio.h>   // plik nagłówkowy dla we/wy standardowego
+#include <stdlib.h>  // plik nagłówkowy dla bibioteki standardowej
+#include <GL/gl.h>   // plik nagłówkowy dla biblioteki OpenGL32
+#include <GL/glu.h>  // plik nagłówkowy dla biblioteki GLu32
+#include <GL/glut.h> // plik nagłówkowy dla biblioteki Glut
+#include <GL/glext.h>
 #include "targa.h"
 #include "stdio.h"
 
@@ -23,12 +24,12 @@
 
 enum
 {
-VENUS_TEX, // Wenus
-EARTH_TEX, // Ziemia
-MARS_TEX, // Mars
-FULL_WINDOW, // aspekt obrazu - całe okno
-ASPECT_1_1, // aspekt obrazu 1:1
-EXIT // wyjście
+	VENUS_TEX,   // Wenus
+	EARTH_TEX,   // Ziemia
+	MARS_TEX,	// Mars
+	FULL_WINDOW, // aspekt obrazu - całe okno
+	ASPECT_1_1,  // aspekt obrazu 1:1
+	EXIT		 // wyjście
 };
 
 // aspekt obrazu
@@ -61,96 +62,85 @@ int button_x, button_y;
 
 GLfloat scale = 3.5;
 
-typedef struct point_3d { // struktura dla punktu 3D
+typedef struct point_3d
+{ // struktura dla punktu 3D
 
-double x, y, z;
+	double x, y, z;
 
 } POINT_3D;
 
+typedef struct bpatch
+{ // struktura dla wielomiana fragmentu Beziera 3 stopnia
 
+	POINT_3D anchors[4][4]; // siatka 4x4 punktów kontrolnych (anchor)
 
-typedef struct bpatch { // struktura dla wielomiana fragmentu Beziera 3 stopnia
+	GLuint dlBPatch; // lista dla fragmentu Beziera
 
-POINT_3D anchors[4][4]; // siatka 4x4 punktów kontrolnych (anchor)
-
-GLuint dlBPatch; // lista dla fragmentu Beziera
-
-GLuint texture; // tekstura dla fragmentu
+	GLuint texture; // tekstura dla fragmentu
 
 } BEZIER_PATCH;
 
-
-
-HDC hDC = NULL; // kontekst urządzenia
-
-HGLRC hRC = NULL; // kontekst renderingu
-
-HWND hWnd = NULL; // deskryptor okna
-
-HINSTANCE hInstance; // egzemplarz programu
-
-
+//HDC hDC = NULL; // kontekst urządzenia
+//HGLRC hRC = NULL; // kontekst renderingu
+//HWND hWnd = NULL; // deskryptor okna
+//HINSTANCE hInstance; // egzemplarz programu
 
 bool keys[256]; // tablica jednowymiarowa dla klawiatury
 
-bool active = TRUE; // flag of application activity
+bool active = true; // flag of application activity
 
-bool fullscreen = TRUE; // flag of full-screen mode
+bool fullscreen = true; // flag of full-screen mode
 
-
-
-DEVMODE DMsaved; // zachować ustawienia przeszłego trybu
-
-
+//DEVMODE DMsaved; // zachować ustawienia przeszłego trybu
 
 GLfloat rotz = 0.0f; // obroty współrzędnej Z
 
 BEZIER_PATCH mybezier; // fragment Beziera dla wykorzystania
 
-bool showCPoints = TRUE; // switcher of anchor points of net
+bool showCPoints = true; // switcher of anchor points of net
 
 int divs = 7; // number of interpolation (distribution of polygon)
 
-
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // deklaracja dla WndProc
+//LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // deklaracja dla WndProc
 
 //functions for vector operations. It can be used any C++ class for 3D point
 
 // addition of 2 points
 
-POINT_3D pointAdd(POINT_3D p, POINT_3D q) {
+POINT_3D pointAdd(POINT_3D p, POINT_3D q)
+{
 
-p.x += q.x; p.y += q.y; p.z += q.z;
+	p.x += q.x;
+	p.y += q.y;
+	p.z += q.z;
 
-return p;
-
+	return p;
 }
-
-
 
 // multiplication of point by constant
 
-POINT_3D pointTimes(double c, POINT_3D p) {
+POINT_3D pointTimes(double c, POINT_3D p)
+{
 
-p.x *= c; p.y *= c; p.z *= c;
+	p.x *= c;
+	p.y *= c;
+	p.z *= c;
 
-return p;
-
+	return p;
 }
-
-
 
 // making new point
 
-POINT_3D makePoint(double a, double b, double c) {
+POINT_3D makePoint(double a, double b, double c)
+{
 
-POINT_3D p;
+	POINT_3D p;
 
-p.x = a; p.y = b; p.z = c;
+	p.x = a;
+	p.y = b;
+	p.z = c;
 
-return p;
-
+	return p;
 }
 
 //function for calculation of 3rd order polynomial (each counterpart in Bezie curve equation is one of so called
@@ -158,34 +148,26 @@ return p;
 // and to calculate point on curve. Changing u with the same increments between 0 and 1 we get
 // good curve approximation
 
-
-
 // calculate polynomial of 3rd order based on array of 4 points
 
 // and variable u changed from 0 to 1
 
-POINT_3D Bernstein(float u, POINT_3D *p) {
+POINT_3D Bernstein(float u, POINT_3D *p)
+{
 
-POINT_3D a, b, c, d, r;
+	POINT_3D a, b, c, d, r;
 
+	a = pointTimes(pow(u, 3), p[0]);
 
+	b = pointTimes(3 * pow(u, 2) * (1 - u), p[1]);
 
-a = pointTimes(pow(u, 3), p[0]);
+	c = pointTimes(3 * u * pow((1 - u), 2), p[2]);
 
-b = pointTimes(3 * pow(u, 2)*(1 - u), p[1]);
+	d = pointTimes(pow((1 - u), 3), p[3]);
 
-c = pointTimes(3 * u*pow((1 - u), 2), p[2]);
+	r = pointAdd(pointAdd(a, b), pointAdd(c, d));
 
-d = pointTimes(pow((1 - u), 3), p[3]);
-
-
-
-r = pointAdd(pointAdd(a, b), pointAdd(c, d));
-
-
-
-return r;
-
+	return r;
 }
 
 //this function generates all slices of triangles and stores them in display list.
@@ -202,403 +184,346 @@ return r;
 // then to calculate vector product in order to get perpendicular for both axes and then
 // to normalize vector and to use it as normal.
 
+GLuint genBezier(BEZIER_PATCH patch, int divs)
+{
 
+	int u = 0, v;
 
-GLuint genBezier(BEZIER_PATCH patch, int divs) {
+	float py, px, pyold;
 
-int u = 0, v;
+	GLuint drawlist = glGenLists(1);
 
-float py, px, pyold;
+	POINT_3D temp[4];
 
-GLuint drawlist = glGenLists(1);
+	POINT_3D *last = (POINT_3D *)malloc(sizeof(POINT_3D) * (divs + 1));
 
-POINT_3D temp[4];
+	if (patch.dlBPatch != NULL)
 
-POINT_3D *last = (POINT_3D*)malloc(sizeof(POINT_3D)*(divs + 1));
+		glDeleteLists(patch.dlBPatch, 1);
 
+	temp[0] = patch.anchors[0][3];
 
+	temp[1] = patch.anchors[1][3];
 
-if (patch.dlBPatch != NULL)
+	temp[2] = patch.anchors[2][3];
 
-glDeleteLists(patch.dlBPatch, 1);
+	temp[3] = patch.anchors[3][3];
 
+	for (v = 0; v <= divs; v++)
+	{
 
+		px = ((float)v) / ((float)divs);
 
-temp[0] = patch.anchors[0][3];
+		last[v] = Bernstein(px, temp);
+	}
 
-temp[1] = patch.anchors[1][3];
+	glNewList(drawlist, GL_COMPILE);
 
-temp[2] = patch.anchors[2][3];
+	glBindTexture(GL_TEXTURE_2D, patch.texture);
 
-temp[3] = patch.anchors[3][3];
+	for (u = 1; u <= divs; u++)
+	{
 
+		py = ((float)u) / ((float)divs);
 
+		pyold = ((float)u - 1.0f) / ((float)divs);
 
-for (v = 0;v <= divs;v++) {
+		temp[0] = Bernstein(py, patch.anchors[0]);
 
-px = ((float)v) / ((float)divs);
+		temp[1] = Bernstein(py, patch.anchors[1]);
 
+		temp[2] = Bernstein(py, patch.anchors[2]);
 
+		temp[3] = Bernstein(py, patch.anchors[3]);
 
-last[v] = Bernstein(px, temp);
+		glBegin(GL_TRIANGLE_STRIP);
 
+		for (v = 0; v <= divs; v++)
+		{
+
+			px = ((float)v) / ((float)divs);
+
+			glTexCoord2f(pyold, px);
+
+			glVertex3d(last[v].x, last[v].y, last[v].z);
+
+			last[v] = Bernstein(px, temp);
+
+			glTexCoord2f(py, px);
+
+			glVertex3d(last[v].x, last[v].y, last[v].z);
+		}
+
+		glEnd();
+	}
+
+	glEndList();
+
+	free(last);
+
+	return drawlist;
 }
 
+void initBezier(void)
+{
 
+	mybezier.anchors[0][0] = makePoint(-0.75, -0.75, -0.50);
 
-glNewList(drawlist, GL_COMPILE);
+	mybezier.anchors[0][1] = makePoint(-0.25, -0.75, 0.00);
 
-glBindTexture(GL_TEXTURE_2D, patch.texture);
+	mybezier.anchors[0][2] = makePoint(0.25, -0.75, 0.00);
 
+	mybezier.anchors[0][3] = makePoint(0.75, -0.75, -0.50);
 
+	mybezier.anchors[1][0] = makePoint(-0.75, -0.25, -0.75);
 
-for (u = 1;u <= divs;u++) {
+	mybezier.anchors[1][1] = makePoint(-0.25, -0.25, 0.50);
 
-py = ((float)u) / ((float)divs);
+	mybezier.anchors[1][2] = makePoint(0.25, -0.25, 0.50);
 
-pyold = ((float)u - 1.0f) / ((float)divs);
+	mybezier.anchors[1][3] = makePoint(0.75, -0.25, -0.75);
 
+	mybezier.anchors[2][0] = makePoint(-0.75, 0.25, 0.00);
 
+	mybezier.anchors[2][1] = makePoint(-0.25, 0.25, -0.50);
 
-temp[0] = Bernstein(py, patch.anchors[0]);
+	mybezier.anchors[2][2] = makePoint(0.25, 0.25, -0.50);
 
-temp[1] = Bernstein(py, patch.anchors[1]);
+	mybezier.anchors[2][3] = makePoint(0.75, 0.25, 0.00);
 
-temp[2] = Bernstein(py, patch.anchors[2]);
+	mybezier.anchors[3][0] = makePoint(-0.75, 0.75, -0.50);
 
-temp[3] = Bernstein(py, patch.anchors[3]);
+	mybezier.anchors[3][1] = makePoint(-0.25, 0.75, -1.00);
 
+	mybezier.anchors[3][2] = makePoint(0.25, 0.75, -1.00);
 
+	mybezier.anchors[3][3] = makePoint(0.75, 0.75, -0.50);
 
-glBegin(GL_TRIANGLE_STRIP);
-
-
-
-for (v = 0;v <= divs;v++) {
-
-px = ((float)v) / ((float)divs);
-
-
-
-glTexCoord2f(pyold, px);
-
-glVertex3d(last[v].x, last[v].y, last[v].z);
-
-
-
-last[v] = Bernstein(px, temp);
-
-glTexCoord2f(py, px);
-
-glVertex3d(last[v].x, last[v].y, last[v].z);
-
+	mybezier.dlBPatch = NULL;
 }
-
-
-
-glEnd();
-
-}
-
-
-
-glEndList();
-
-
-
-free(last);
-
-return drawlist; /
-
-}
-
-
-
-
-
-void initBezier(void) {
-
-mybezier.anchors[0][0] = makePoint(-0.75, -0.75, -0.50);
-
-mybezier.anchors[0][1] = makePoint(-0.25, -0.75, 0.00);
-
-mybezier.anchors[0][2] = makePoint(0.25, -0.75, 0.00);
-
-mybezier.anchors[0][3] = makePoint(0.75, -0.75, -0.50);
-
-mybezier.anchors[1][0] = makePoint(-0.75, -0.25, -0.75);
-
-mybezier.anchors[1][1] = makePoint(-0.25, -0.25, 0.50);
-
-mybezier.anchors[1][2] = makePoint(0.25, -0.25, 0.50);
-
-mybezier.anchors[1][3] = makePoint(0.75, -0.25, -0.75);
-
-mybezier.anchors[2][0] = makePoint(-0.75, 0.25, 0.00);
-
-mybezier.anchors[2][1] = makePoint(-0.25, 0.25, -0.50);
-
-mybezier.anchors[2][2] = makePoint(0.25, 0.25, -0.50);
-
-mybezier.anchors[2][3] = makePoint(0.75, 0.25, 0.00);
-
-mybezier.anchors[3][0] = makePoint(-0.75, 0.75, -0.50);
-
-mybezier.anchors[3][1] = makePoint(-0.25, 0.75, -1.00);
-
-mybezier.anchors[3][2] = makePoint(0.25, 0.75, -1.00);
-
-mybezier.anchors[3][3] = makePoint(0.75, 0.75, -0.50);
-
-mybezier.dlBPatch = NULL;
-
-}
-
-
-
-
 
 int InitGL(GLvoid)
 
 {
 
-glEnable(GL_TEXTURE_2D);
+	glEnable(GL_TEXTURE_2D);
 
-glShadeModel(GL_SMOOTH);
+	glShadeModel(GL_SMOOTH);
 
-glClearColor(1.00f, 1.00f, 1.00f, 0.5f); // Фон biały
+	glClearColor(1.00f, 1.00f, 1.00f, 0.5f); // Фон biały
 
-glClearDepth(1.0f);
+	glClearDepth(1.0f);
 
-glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
-glDepthFunc(GL_LEQUAL);
+	glDepthFunc(GL_LEQUAL);
 
-glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
+	initBezier();
 
+	// wczytanie tekstury Venus utworzonej ze zdjęć sondy Magellan
+	// plik: http://maps.jpl.nasa.gov/pix/ven0aaa2.tif
+	GLsizei width_texture, height_texture; // zmienne użyte przy obsłudze plików TARGA
+	GLenum format, type;
+	GLvoid *Ptr_mybezier_texture;
+	GLboolean error = load_targa("water_plain_sea_4_20131007_1254588998.tga", width_texture, height_texture, format, type, Ptr_mybezier_texture);
 
-initBezier();
+	// błąd odczytu pliku
+	if (error == GL_FALSE)
+	{
+		printf("Niepoprawny odczyt pliku mar0kuu2.tga");
+		exit(0);
+	}
 
-// wczytanie tekstury Venus utworzonej ze zdjęć sondy Magellan
-// plik: http://maps.jpl.nasa.gov/pix/ven0aaa2.tif
-GLsizei width_texture, height_texture; // zmienne użyte przy obsłudze plików TARGA
-GLenum format, type;
-GLvoid *Ptr_mybezier_texture;
-GLboolean error = load_targa("water_plain_sea_4_20131007_1254588998.tga", width_texture, height_texture, format, type, Ptr_mybezier_texture);
+	// dowiązanie stanu tekstury
+	glBindTexture(GL_TEXTURE_2D, (GLuint)Ptr_mybezier_texture);
 
+	// utworzenie tekstury wraz z mipmapami
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, width_texture, height_texture, format, type, Ptr_mybezier_texture);
 
-// błąd odczytu pliku
-if (error == GL_FALSE)
-{
-printf("Niepoprawny odczyt pliku mar0kuu2.tga");
-exit(0);
+	///////////////////
+	mybezier.texture = (GLuint)Ptr_mybezier_texture;
+	///////////////////
+	mybezier.dlBPatch = genBezier(mybezier, divs);
+
+	// porządki
+	delete[](GLvoid *) Ptr_mybezier_texture;
+
+	return true;
 }
 
-
-// dowiązanie stanu tekstury
-glBindTexture(GL_TEXTURE_2D, (GLuint)Ptr_mybezier_texture);
-
-// utworzenie tekstury wraz z mipmapami
-gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, width_texture, height_texture, format, type, Ptr_mybezier_texture);
-
-///////////////////
-mybezier.texture = (GLuint)Ptr_mybezier_texture;
-///////////////////
-mybezier.dlBPatch = genBezier(mybezier, divs);
-
-// porządki
-delete[](GLvoid*)Ptr_mybezier_texture;
-
-return TRUE;
-
-}
-
-
-
-
-void DrawGLScene(GLvoid) {
-
-int i, j;
-
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-////////////////////
-glLoadIdentity();
-
-
-// obroty obiektu
-glRotatef(-75.0f, 1.0f, 0.0f, 0.0f);
-
-
-// przesunięcie układu współrzędnych obiektów do środka bryły odcinania
-glTranslatef(0.0, 0.0, -(near_ + far_) / 2);
-
-// obroty obiektu
-glRotatef(rotatex, 1.0, 0.0, 0.0);
-glRotatef(rotatez, 0.0, 0.0, 1.0);
-
-// skalowanie obiektu - klawisze "+" i "-"
-glScalef(scale, scale, scale);
-
-
-glCallList(mybezier.dlBPatch);
-
-
-
-
-BOOL showCPoint = TRUE;
-if (showCPoints)
+void DrawGLScene(GLvoid)
 {
 
-glDisable(GL_TEXTURE_2D);
+	int i, j;
 
-glColor3f(0.0f, 1.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-// rozmiar punktów
-glPointSize(6.0);
+	////////////////////
+	glLoadIdentity();
 
-for (i = 0;i<4;i++) {
+	// obroty obiektu
+	glRotatef(-75.0f, 1.0f, 0.0f, 0.0f);
 
-glBegin(GL_LINE_STRIP);
+	// przesunięcie układu współrzędnych obiektów do środka bryły odcinania
+	glTranslatef(0.0, 0.0, -(near_ + far_) / 2);
 
+	// obroty obiektu
+	glRotatef(rotatex, 1.0, 0.0, 0.0);
+	glRotatef(rotatez, 0.0, 0.0, 1.0);
 
-for (j = 0;j<4;j++)
+	// skalowanie obiektu - klawisze "+" i "-"
+	glScalef(scale, scale, scale);
 
-glVertex3d(mybezier.anchors[i][j].x, mybezier.anchors[i][j].y, mybezier.anchors[i][j].z);
+	glCallList(mybezier.dlBPatch);
 
-glEnd();
+	bool showCPoint = true;
+	if (showCPoints)
+	{
 
-}
+		glDisable(GL_TEXTURE_2D);
 
-for (i = 0;i<4;i++) {
+		glColor3f(0.0f, 1.0f, 0.0f);
 
-glBegin(GL_LINE_STRIP);
+		// rozmiar punktów
+		glPointSize(6.0);
 
+		for (i = 0; i < 4; i++)
+		{
 
-for (j = 0;j<4;j++)
+			glBegin(GL_LINE_STRIP);
 
-glVertex3d(mybezier.anchors[j][i].x, mybezier.anchors[j][i].y, mybezier.anchors[j][i].z);
+			for (j = 0; j < 4; j++)
 
-glEnd();
+				glVertex3d(mybezier.anchors[i][j].x, mybezier.anchors[i][j].y, mybezier.anchors[i][j].z);
 
-}
+			glEnd();
+		}
 
-glColor3f(1.0f, 1.0f, 1.0f);
+		for (i = 0; i < 4; i++)
+		{
 
-glEnable(GL_TEXTURE_2D);
+			glBegin(GL_LINE_STRIP);
 
-}
+			for (j = 0; j < 4; j++)
 
-// wyświetlenie sceny
-glutPostRedisplay();
+				glVertex3d(mybezier.anchors[j][i].x, mybezier.anchors[j][i].y, mybezier.anchors[j][i].z);
 
-//return TRUE;
-glFlush();
+			glEnd();
+		}
 
-glutSwapBuffers();
+		glColor3f(1.0f, 1.0f, 1.0f);
 
+		glEnable(GL_TEXTURE_2D);
+	}
+
+	// wyświetlenie sceny
+	glutPostRedisplay();
+
+	//return true;
+	glFlush();
+
+	glutSwapBuffers();
 }
 
 void resize(int width, int height)
 {
-glViewport(0, 0, width, height);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-glFrustum(-5, 5, -5, 5, 2, 12);
-gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-glMatrixMode(GL_MODELVIEW);
-DrawGLScene();
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-5, 5, -5, 5, 2, 12);
+	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+	glMatrixMode(GL_MODELVIEW);
+	DrawGLScene();
 }
-
-
 
 // obsługa przycisków myszki
 
 void MouseButton(int button, int state, int x, int y)
 {
-if (button == GLUT_LEFT_BUTTON)
-{
-// zapamiętanie stanu lewego przycisku myszki
-button_state = state;
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		// zapamiętanie stanu lewego przycisku myszki
+		button_state = state;
 
-// zapamiętanie położenia kursora myszki
-if (state == GLUT_DOWN)
-{
-button_x = x;
-button_y = y;
-}
-}
+		// zapamiętanie położenia kursora myszki
+		if (state == GLUT_DOWN)
+		{
+			button_x = x;
+			button_y = y;
+		}
+	}
 }
 
 // obsługa ruchu kursora myszki
 
 void MouseMotion(int x, int y)
 {
-if (button_state == GLUT_DOWN)
-{
-rotatez += 30 * (right - left) / glutGet(GLUT_WINDOW_WIDTH) * (x - button_x);
-button_x = x;
-rotatex -= 30 * (top - bottom) / glutGet(GLUT_WINDOW_HEIGHT) * (button_y - y);
-button_y = y;
-glutPostRedisplay();
-}
+	if (button_state == GLUT_DOWN)
+	{
+		rotatez += 30 * (right - left) / glutGet(GLUT_WINDOW_WIDTH) * (x - button_x);
+		button_x = x;
+		rotatex -= 30 * (top - bottom) / glutGet(GLUT_WINDOW_HEIGHT) * (button_y - y);
+		button_y = y;
+		glutPostRedisplay();
+	}
 }
 
 void Keyboard(unsigned char key, int x, int y)
 {
-// klawisz +
-if (key == '+')
-scale += 0.05;
-else
+	// klawisz +
+	if (key == '+')
+		scale += 0.05;
+	else
 
-// klawisz -
-if (key == '-' && scale > 0.05)
-scale -= 0.05;
+		// klawisz -
+		if (key == '-' && scale > 0.05)
+		scale -= 0.05;
 
-// narysowanie sceny
-DrawGLScene();
+	// narysowanie sceny
+	DrawGLScene();
 }
 
 int main(int argc, char *argv[])
 {
-// inicjalizacja biblioteki GLUT
-glutInit(&argc, argv);
+	// inicjalizacja biblioteki GLUT
+	glutInit(&argc, argv);
 
-// inicjalizacja bufora ramki
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	// inicjalizacja bufora ramki
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-glutInitWindowPosition(50, 10);
+	glutInitWindowPosition(50, 10);
 
-// rozmiary głównego okna programu
-glutInitWindowSize(500, 500);
+	// rozmiary głównego okna programu
+	glutInitWindowSize(500, 500);
 
-// utworzenie głównego okna programu
-glutCreateWindow("Krzywa Beziera");
+	// utworzenie głównego okna programu
+	glutCreateWindow("Krzywa Beziera");
 
-// dołączenie funkcji generującej scenę 3D
-glutDisplayFunc(DrawGLScene);
+	// dołączenie funkcji generującej scenę 3D
+	glutDisplayFunc(DrawGLScene);
 
-// dołączenie funkcji wywoływanej przy zmianie rozmiaru okna
-glutReshapeFunc(resize);
-//glutReshapeFunc(Reshape);
+	// dołączenie funkcji wywoływanej przy zmianie rozmiaru okna
+	glutReshapeFunc(resize);
+	//glutReshapeFunc(Reshape);
 
-// dołączenie funkcji obsługi klawiatury
-glutKeyboardFunc(Keyboard);
+	// dołączenie funkcji obsługi klawiatury
+	glutKeyboardFunc(Keyboard);
 
-// obsługa przycisków myszki
-glutMouseFunc(MouseButton);
+	// obsługa przycisków myszki
+	glutMouseFunc(MouseButton);
 
-// obsługa ruchu kursora myszki
-glutMotionFunc(MouseMotion);
+	// obsługa ruchu kursora myszki
+	glutMotionFunc(MouseMotion);
 
-InitGL();
+	InitGL();
 
-// wprowadzenie programu do obsługi pętli komunikatów
-glutMainLoop();
+	// wprowadzenie programu do obsługi pętli komunikatów
+	glutMainLoop();
 
-// porządki
-delete[](GLvoid*)mybezier.texture;
+	// porządki
+	delete[](GLvoid *) mybezier.texture;
 
-return 0;
+	return 0;
 }
